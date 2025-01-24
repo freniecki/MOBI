30006b0084ef8537d71b5eac955e4f6114da2351
freniecki <247773@edu.p.lodz.pl>
2025-01-24 21:09:26 +0100

    qr code in orders

--- app/src/main/java/pl/mobi/ui/adapters/OrderAdapter.java
+++ app/src/main/java/pl/mobi/ui/adapters/OrderAdapter.java
@@ -1,14 +1,23 @@
 package pl.mobi.ui.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,14 +25,16 @@ import java.util.List;
 import java.util.Locale;
 
 import pl.mobi.R;
-import pl.mobi.ui.models.CartItem;
 import pl.mobi.ui.models.Order;
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
     private Context context;
     private List<Order> orderList;
+    private FirebaseFirestore db;
+    private FirebaseUser firebaseUser;
 
     public OrderAdapter(Context context, List<Order> orderList) {
+        db = FirebaseFirestore.getInstance();
         this.context = context;
         this.orderList = orderList;
     }
@@ -38,9 +49,28 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Order order = orderList.get(position);
+
+        db.collection("users")
+                .document(order.getChildId())
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        holder.child.setText("Dziecko: " + documentSnapshot.getString("email"));
+                    } else {
+                        holder.child.setText("Dziecko: nie-znaleziono emaila na bazie id");
+                    }
+                });
+        db.collection("users").document(order.getParentId())
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        holder.parent.setText("Rodzic: " + documentSnapshot.getString("email"));
+                    } else {
+                        holder.parent.setText("Rodzic: nie-znaleziono emaila na bazie id");
+                    }
+                });
+
         holder.status.setText("Status: " + order.getStatus());
-        holder.child.setText("Dziecko: " + order.getChildId());
-        holder.parent.setText("Rodzic: " + order.getParentId());
 
         // Set up nested RecyclerView for products
         OrderProductAdapter productAdapter = new OrderProductAdapter(context, order.getItems());
@@ -53,6 +83,21 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
             String formattedDate = sdf.format(date); // Format the Date
             holder.pickupDate.setText("Data odbioru: " + formattedDate); // Display formatted date
         }
+        String orderId = order.getOrderId();
+        Bitmap qrCodeBitmap = generateQRCode(orderId);
+        holder.qrCodeImageView.setImageBitmap(qrCodeBitmap);
+    }
+
+    public Bitmap generateQRCode(String orderId) {
+        try {
+            if (orderId == null || orderId.isEmpty()) {
+                return BitmapFactory.decodeResource(context.getResources(), R.drawable.minus);
+            }
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            return barcodeEncoder.encodeBitmap(orderId, BarcodeFormat.QR_CODE, 200, 200);
+        } catch (WriterException e) {
+            return BitmapFactory.decodeResource(context.getResources(), R.drawable.plus);
+        }
     }
 
     @Override
@@ -61,6 +106,7 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView qrCodeImageView;
         TextView pickupDate, status, parent, child;
         RecyclerView recyclerViewProducts;
 
@@ -71,6 +117,7 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
             child = itemView.findViewById(R.id.childName);
             parent = itemView.findViewById(R.id.parentName);
             recyclerViewProducts = itemView.findViewById(R.id.recyclerViewProducts);
+            qrCodeImageView = itemView.findViewById(R.id.qrCodeImageView);
         }
     }
 }
